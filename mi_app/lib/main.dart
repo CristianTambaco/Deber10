852294b33b6ae05900d957fe9e0b99ae316e7d25
CristianTambaco <1234cristiant@gmail.com>
2025-12-13 00:44:09 -0500

    new file:   mi_app/.env 	new file:   mi_app/.env.example 	modified:   mi_app/android/gradle/wrapper/gradle-wrapper.properties 	modified:   mi_app/android/settings.gradle 	new file:   mi_app/lib/core/error/exceptions.dart 	new file:   mi_app/lib/core/error/failures.dart 	new file:   mi_app/lib/core/theme/app_theme.dart 	new file:   mi_app/lib/core/usecases/usecase.dart 	new file:   mi_app/lib/core/utils/constants.dart 	new file:   mi_app/lib/core/utils/validators.dart 	new file:   mi_app/lib/features/auth/data/datasources/auth_remote_datasource.dart 	new file:   mi_app/lib/features/auth/data/models/user_model.dart 	new file:   mi_app/lib/features/auth/data/repositories/auth_repository_impl.dart 	new file:   mi_app/lib/features/auth/domain/entities/user_entity.dart 	new file:   mi_app/lib/features/auth/domain/repositories/auth_repository.dart 	new file:   mi_app/lib/features/auth/domain/usecases/get_current_user_usecase.dart 	new file:   mi_app/lib/features/auth/domain/usecases/send_password_reset_usecase.dart 	new file:   mi_app/lib/features/auth/domain/usecases/sign_in_usecase.dart 	new file:   mi_app/lib/features/auth/domain/usecases/sign_out_usecase.dart 	new file:   mi_app/lib/features/auth/domain/usecases/sign_up_usecase.dart 	new file:   mi_app/lib/features/auth/domain/usecases/update_password_usecase.dart 	new file:   mi_app/lib/features/auth/presentation/bloc/auth_bloc.dart 	new file:   mi_app/lib/features/auth/presentation/bloc/auth_event.dart 	new file:   mi_app/lib/features/auth/presentation/bloc/auth_state.dart 	new file:   mi_app/lib/features/auth/presentation/pages/change_password_page.dart 	new file:   mi_app/lib/features/auth/presentation/pages/forgot_password_page.dart 	new file:   mi_app/lib/features/auth/presentation/pages/home_page.dart 	new file:   mi_app/lib/features/auth/presentation/pages/login_page.dart 	new file:   mi_app/lib/features/auth/presentation/pages/register_page.dart 	new file:   mi_app/lib/features/auth/presentation/pages/splash_page.dart 	new file:   mi_app/lib/features/auth/presentation/widgets/auth_button.dart 	new file:   mi_app/lib/features/auth/presentation/widgets/auth_text_field.dart 	new file:   mi_app/lib/features/auth/presentation/widgets/loading_overlay.dart 	new file:   mi_app/lib/injection_container.dart 	modified:   mi_app/lib/main.dart 	new file:   mi_app/lib/theme/app_theme.dart 	new file:   mi_app/lib/usecases/usecase.dart 	new file:   mi_app/lib/utils/constants.dart 	new file:   mi_app/lib/utils/validators.dart 	modified:   mi_app/linux/flutter/generated_plugin_registrant.cc 	modified:   mi_app/linux/flutter/generated_plugins.cmake 	modified:   mi_app/macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   mi_app/pubspec.lock 	modified:   mi_app/pubspec.yaml 	modified:   mi_app/windows/flutter/generated_plugin_registrant.cc 	modified:   mi_app/windows/flutter/generated_plugins.cmake

--- mi_app/lib/main.dart
+++ mi_app/lib/main.dart
@@ -4,8 +4,19 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
 import 'screens/home_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'injection_container.dart' as di; // Asegúrate de haber copiado injection_container.dart
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+  await di.initDependencies(); // Inicializa GetIt con el AuthBloc y servicios
   runApp(const MyApp());
 }
 
